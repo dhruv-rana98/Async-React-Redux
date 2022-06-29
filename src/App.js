d1beba9918ece403b33ddc91b5ade90913efe0f6
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-29 16:51:51 +0530

    Moving the async code to cart-actions to create a thunk and addition of new thunk to cart-actions to handle the side effect of fetching data from firebase using get request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { sendCartData } from "./store/cart-slice";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -15,11 +15,17 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (initial) {
       initial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
   return (
     <Fragment>
